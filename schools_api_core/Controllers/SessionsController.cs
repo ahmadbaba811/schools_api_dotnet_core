4b3bfd68b69b4e55d8fb920db9817242738f5448
ahmadbaba811 <ahmadbaba811@hotmail.com>
2022-12-21 17:57:04 +0100

    added subject, class, term, session routes

---
+++ schools_api_core/Controllers/SessionsController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using schools_api_core.Data;
+using schools_api_core.Models;
+
+namespace schools_api_core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionsController : ControllerBase
+    {
+        private readonly schoolDbContext _context;
+        public SessionsController(schoolDbContext context) => _context = context;
+
+        //GET ALL SESSION LIST
+        [HttpGet("session-list")]
+        public async Task<IEnumerable<TblSession>> Get()
+        {
+            return await _context.TblSessions.ToListAsync();
+        }
+
+        //GET ALL SESSION BY ROW ID
+        [HttpGet("session/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var session = await _context.TblSessions.FindAsync(id);
+            if (session == null) return BadRequest("no record");
+            return Ok(session);
+        }
+
+        //GET ALL SESSION BY SESSION NAME
+        [HttpPost("session-by-name")]
+        public async Task<IActionResult> GetBySessionName(TblSession se)
+        {
+            var session = await _context.TblSessions.Where(x => x.SessionName == se.SessionName).FirstOrDefaultAsync();
+            if (session == null) return BadRequest("no record");
+            return Ok(session);
+        }
+
+        //GET ACTIVE SESSION
+        [HttpGet("active-session")]
+        public async Task<IActionResult> GetActiveSession()
+        {
+            var session = await _context.TblSessions.Where(x => x.Status == "1").FirstOrDefaultAsync();
+            if (session == null) return BadRequest("no record");
+            return Ok(session);
+        }
+
+        //ADD NEW SESSION
+        [HttpPost("add-session")]
+        public async Task<IActionResult> CreateSession(TblSession session)
+        {
+            var exisitingSeession = _context.TblSessions.Where(x => x.SessionName == session.SessionName).FirstOrDefault();
+            if (exisitingSeession != null) return BadRequest("session exists");
+
+            await _context.TblSessions.AddAsync(session);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = session.Id }, session);
+        }
+
+        //DELETE SESSION
+        [HttpDelete("delete-session/{id}")]
+        public async Task<IActionResult> DeleteSesssion(int id, TblSession session)
+        {
+            var sessionToDelete = await _context.TblSessions.FindAsync(id);
+            if (sessionToDelete == null) return BadRequest("no record");
+
+            _context.TblSessions.Remove(sessionToDelete);
+            await _context.SaveChangesAsync();
+            return Ok("deleted");
+        }
+
+        //UPDATE SESSION
+        [HttpPut("update-session/{id}")]
+        public async Task<IActionResult> UpdateSession(int id, TblSession session)
+        {
+            if (session.Status == "1")
+            {
+                var activeSession = await _context.TblSessions.Where(x => x.Status == "1").FirstOrDefaultAsync();
+                if (activeSession != null) return BadRequest("active session exists");
+            }
+
+            var ss = await _context.TblSessions.FindAsync(id);
+            if (ss == null) return BadRequest("no record");
+
+            if (ss != null)
+            {
+                ss.Id = session.Id;
+                ss.SessionName = session.SessionName;
+                ss.StartDate = session.StartDate;
+                ss.EndDate = session.EndDate;
+                ss.Status = session.Status;
+                ss.AllowPromotion = session.AllowPromotion;
+                ss.AddedBy = session.AddedBy;
+                ss.AddedDate = session.AddedDate;
+                await _context.SaveChangesAsync();
+            }
+            return Ok("updated");
+        }
+    }
+}
---
+++ schools_api_core/Controllers/SubjectController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using schools_api_core.Data;
+using schools_api_core.Models;
+
+namespace schools_api_core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubjectController : ControllerBase
+    {
+        private readonly schoolDbContext _context;
+        public SubjectController(schoolDbContext context) => _context = context;
+
+        //GET ALL SUBJECT LIST
+        [HttpGet("subject-list")]
+        public async Task<IEnumerable<TblSubject>> Get()
+        {
+            return await _context.TblSubjects.ToListAsync();
+        }
+
+
+        //GET ALL SUBJECT BY ROW ID
+        [HttpGet("subject/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var subject = await _context.TblSubjects.FindAsync(id);
+            if (subject == null) return BadRequest("no record");
+            return Ok(subject);
+        }
+
+
+        //GET ALL SUBJECT BY SUBJECT NAME
+        [HttpGet("subject-by-name/{subject_name}")]
+        public async Task<IActionResult> GetSubjectByName(string subject_name)
+        {
+            var subject = await _context.TblSubjects.Where(x => x.SubjectName == subject_name).FirstOrDefaultAsync();
+            if (subject == null) return BadRequest("no record");
+            return Ok(subject);
+        }
+
+
+        //ADD NEW SUBJECT
+        [HttpPost("add-subject")]
+        public async Task<IActionResult> CreateSubject(TblSubject subject)
+        {
+            var exisitingSubject = _context.TblSubjects.Where(x => x.SubjectName == subject.SubjectName).FirstOrDefault();
+            if (exisitingSubject != null) return BadRequest("subject exists");
+
+            await _context.TblSubjects.AddAsync(subject);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = subject.Id }, subject);
+        }
+
+        //DELETE SUBJECT
+        [HttpDelete("delete-subject/{id}")]
+        public async Task<IActionResult> DeleteSubject(int id)
+        {
+            var subjectToDelete = await _context.TblSubjects.FindAsync(id);
+            if (subjectToDelete == null) return BadRequest("no record");
+
+            _context.TblSubjects.Remove(subjectToDelete);
+            await _context.SaveChangesAsync();
+            return Ok("deleted");
+        }
+
+
+        //UPDATE SUBJECT
+        [HttpPut("update-subject/{id}")]
+        public async Task<IActionResult> UpdateSubject(int id, TblSubject _subject)
+        {
+            var tt = await _context.TblSubjects.FindAsync(id);
+            if (tt == null) return BadRequest("no record");
+
+            if (tt != null)
+            {
+                tt.SubjectName = _subject.SubjectName;
+                tt.SubjectCategory = _subject.SubjectCategory;
+                tt.AddedBy = _subject.AddedBy;
+                tt.AddedDate = _subject.AddedDate;
+                await _context.SaveChangesAsync();
+            }
+            return Ok("updated");
+        }
+
+
+        //GET ALL SUBJECT GROUP
+        [HttpGet("subject-group-list")]
+        public async Task<IEnumerable<TblSubjectGroup>> GetSubjectGroup()
+        {
+            return await _context.TblSubjectGroups.ToListAsync();
+        }
+
+
+        //GET ALL SUBJECT GROUP BY GROUPCODE
+        [HttpGet("subject-group-by-name/{group_code}")]
+        public async Task<IActionResult> GetSubjectGroupByCode(string group_code)
+        {
+            var subject_group = await _context.TblSubjectGroups.Where(x => x.GroupCode == group_code).ToListAsync();
+            if (subject_group == null) return BadRequest("no record");
+            return Ok(subject_group);
+        }
+
+        //GET ALL SUBJECT GROUP BY ROW ID
+        [HttpGet("subject-group/{id}")]
+        public async Task<IActionResult> GetBySubjectId(int id)
+        {
+            var subject_group = await _context.TblSubjectGroups.FindAsync(id);
+            if (subject_group == null) return BadRequest("no record");
+            return Ok(subject_group);
+        }
+
+
+        //ADD NEW SUBJECT GROUPING
+        [HttpPost("add-subject-group")]
+        public async Task<IActionResult> CreateSubjectGroup(TblSubjectGroup subject)
+        {
+            var exisitingSubjectGroup = _context.TblSubjectGroups.Where(x => x.SubjectName == subject.SubjectName && x.GroupCode == subject.GroupCode).FirstOrDefault();
+            if (exisitingSubjectGroup != null) return BadRequest("group exists");
+
+            await _context.TblSubjectGroups.AddAsync(subject);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBySubjectId), new { id = subject.Id }, subject);
+        }
+
+
+        //DELETE SUBJECT GROUPS
+        [HttpDelete("delete-subject-group/{id}")]
+        public async Task<IActionResult> DeleteSubjectGroups(int id)
+        {
+            var subjectGroupToDelete = await _context.TblSubjectGroups.FindAsync(id);
+            if (subjectGroupToDelete == null) return BadRequest("no record");
+
+            _context.TblSubjectGroups.Remove(subjectGroupToDelete);
+            await _context.SaveChangesAsync();
+            return Ok("deleted");
+        }
+
+        
+
+
+    }
+}

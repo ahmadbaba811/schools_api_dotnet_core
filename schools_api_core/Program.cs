77836233746855243187fec7ac63e5919c5f93fc
ahmadbaba811 <ahmadbaba811@hotmail.com>
2022-12-30 21:07:01 +0100

    u

--- schools_api_core/Program.cs
+++ schools_api_core/Program.cs
@@ -9,8 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//to use use sql
 builder.Services.AddDbContext<schoolDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("schCon")));
 
+//to allow CORS
 builder.Services.AddCors(p => p.AddPolicy("schCors", builder =>
 {
     builder.WithOrigins("*").AllowAnyOrigin().AllowAnyHeader();

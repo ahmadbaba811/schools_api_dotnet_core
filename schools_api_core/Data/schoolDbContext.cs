7f5b65f57c3ad00d6eb01dec448dad9111d05566
ahmadbaba811 <ahmadbaba811@hotmail.com>
2023-02-21 18:41:37 +0100

    u

--- schools_api_core/Data/schoolDbContext.cs
+++ schools_api_core/Data/schoolDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-using schools_api_core.Controllers;
 using schools_api_core.Models;
 
 namespace schools_api_core.Data;
@@ -79,11 +78,9 @@ public partial class schoolDbContext : DbContext
 
     public virtual DbSet<VwSubjectMaxMin> VwSubjectMaxMins { get; set; }
 
-    public virtual DbSet<TblAddSubjects> TblAddSubjects { get; set; }
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-JDPULFC\\JIBRILMUHAMMAD;Initial Catalog=schooldb_a121w;User ID=sa;Password=edati_jibril; Encrypt= false");
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-NL26F2F\\SQL_SERVER;Initial Catalog=schooldb_a121w;User ID=sa;Password=a; Encrypt= false");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -260,15 +257,6 @@ public partial class schoolDbContext : DbContext
             entity.Property(e => e.ClassName).IsFixedLength();
             entity.Property(e => e.SessionName).IsFixedLength();
             entity.Property(e => e.TermName).IsFixedLength();
-        });
-
-        modelBuilder.Entity<TblAddSubjects>(entity =>
-        {
-            entity.Property(e => e.Subject).IsFixedLength();
-            entity.Property(e => e.added_by).IsFixedLength();
-            entity.HasKey(e => e.id).HasName("PK_Subject");
-
-            entity.Property(e => e.date_added).HasDefaultValueSql("(getdate())");
         });
 
         modelBuilder.Entity<VwStudentResult>(entity =>
